30d98a9d3f9203d6da9524e99e7fc77290e16c35
alif33 <alifhasan332@gmail.com>
2020-12-24 19:57:24 +0600

    24-12-20

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const fs = require('fs');
+const cors = require('cors')
+const fetch = require('node-fetch');
+const fb  = require('firebase')
+const app  = express()
+
+
+
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+
+    const url = "https://img.youtube.com/vi/nB-9WsIPe3Y/sddefault.jpg"
+    async function download() {
+        const response = await fetch(url);
+        const buffer = await response.buffer();
+        fs.writeFile(`./public/img/hello.jpg`, buffer, () =>{
+          // console.log(fb)
+        } 
+      );
+      }          
+      download() 
+     res.sendFile(`./public/img/image.jpg`, { root: __dirname })   
+     fs.unlink(`${__dirname}/.public/img/image.jpg`, ()=>{
+       console.log('Everything okay!!')
+     })
+})
+
+
+
+const PORT = process.env.PORT || 4000
+    app.listen(PORT, ()=>{
+        console.log(`SERVER IS RUNNING ON ${PORT}`)
+})
+
+

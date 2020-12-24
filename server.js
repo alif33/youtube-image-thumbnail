b17be13e5285feb4823e3319d4f3971275ca07c0
alif33 <alifhasan332@gmail.com>
2020-12-24 20:34:59 +0600

    ...

--- server.js
+++ server.js
@@ -30,6 +30,23 @@ app.get('/', (req, res)=>{
     //    console.log('Everything okay!!')
     //  })
 })
+app.post('/image', (req, res)=>{
+
+    const url = "https://img.youtube.com/vi/nB-9WsIPe3Y/sddefault.jpg"
+    async function download() {
+        const response = await fetch(url);
+        const buffer = await response.buffer();
+        fs.writeFile(`./public/img/hello.jpg`, buffer, () =>{
+          // console.log(fb)
+        } 
+      );
+      }          
+      download() 
+     res.sendFile(`./public/img/image.jpg`, { root: __dirname })   
+    //  fs.unlink(`${__dirname}/.public/img/image.jpg`, ()=>{
+    //    console.log('Everything okay!!')
+    //  })
+})
 
 
 
